5c84847ea2263b906cb28e07f522e554eb9deedc
Blackmoree <lhy20070208@163.com>
2026-03-11 19:52:34 +0800

    finish task 8

--- proj0/src/Particle.java
+++ proj0/src/Particle.java
@@ -1,4 +1,5 @@
 import edu.princeton.cs.algs4.StdRandom;
+import net.sf.saxon.expr.GeneralComparison;
 
 import java.awt.*;
 import java.util.Map;
@@ -17,7 +18,7 @@ public class Particle {
 
     public Particle(ParticleFlavor flavor) {
         this.flavor = flavor;
-        lifespan = -1;
+        lifespan = -1; // All human created particles have no lifespan
     }
 
     public Color color() {
@@ -38,21 +39,82 @@ public class Particle {
         other.lifespan = this.lifespan;
         this.flavor = ParticleFlavor.EMPTY;
         this.lifespan = -1;
-
     }
 
     public void fall(Map<Direction, Particle> neighbors) {
+        Particle p = neighbors.get(Direction.DOWN);
+        if (p.flavor == ParticleFlavor.EMPTY) {
+            this.moveInto(p);
+        }
     }
 
     public void flow(Map<Direction, Particle> neighbors) {
+        int random_number = StdRandom.uniformInt(3);
+        switch(random_number) {
+            case 0 -> {
+                return;
+            }
+            case 1 -> {
+                Particle left_neighbor = neighbors.get(Direction.LEFT);
+                if (left_neighbor.flavor == ParticleFlavor.EMPTY) {
+                    moveInto(left_neighbor);
+                }
+            }
+            case 2 -> {
+                Particle right_neighbor = neighbors.get(Direction.RIGHT);
+                if (right_neighbor.flavor == ParticleFlavor.EMPTY) {
+                    moveInto(right_neighbor);
+                }
+            }
+        }
     }
 
     public void grow(Map<Direction, Particle> neighbors) {
+        assert(this.flavor == ParticleFlavor.PLANT || this.flavor == ParticleFlavor.FLOWER);
+        //Only plants and flowers can grow.
+
+        int random_number = StdRandom.uniformInt(10);
+        switch(random_number) {
+            case 0 -> {
+                Particle up_neighbor = neighbors.get(Direction.UP);
+                if (up_neighbor.flavor == ParticleFlavor.EMPTY) {
+                    up_neighbor.flavor = this.flavor;
+                    up_neighbor.lifespan = LIFESPANS.get(this.flavor); // Not secure!
+                }
+            }
+            case 1 -> {
+                Particle left_neighbor = neighbors.get(Direction.LEFT);
+                if (left_neighbor.flavor == ParticleFlavor.EMPTY) {
+                    left_neighbor.flavor = this.flavor;
+                    left_neighbor.lifespan = LIFESPANS.get(this.flavor);
+                }
+            }
+            case 2 -> {
+                Particle right_neighbor = neighbors.get(Direction.RIGHT);
+                if (right_neighbor.flavor == ParticleFlavor.EMPTY) {
+                    right_neighbor.flavor = this.flavor;
+                    right_neighbor.lifespan = LIFESPANS.get(this.flavor);
+                }
+            }
+            //default: do nothing
+        }
     }
 
     public void burn(Map<Direction, Particle> neighbors) {
     }
 
     public void action(Map<Direction, Particle> neighbors) {
+        if (this.flavor == ParticleFlavor.EMPTY) {
+            return;
+        }
+        if (this.flavor != ParticleFlavor.BARRIER) {
+            this.fall(neighbors);
+        }
+        if (this.flavor == ParticleFlavor.WATER) {
+            this.flow(neighbors);
+        }
+        if (this.flavor == ParticleFlavor.PLANT || this.flavor == ParticleFlavor.FLOWER) {
+            this.grow(neighbors);
+        }
     }
 }
