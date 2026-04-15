a4cf30fc22f4c147ddfe7e30de997ef0cdde90ab
Blackmoree <lhy20070208@163.com>
2026-04-15 14:26:06 +0800

    Proj 3 backwash solved

--- proj3/src/Percolation.java
+++ proj3/src/Percolation.java
@@ -2,22 +2,24 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
     private int N;
-    private WeightedQuickUnionUF uf;
+    private WeightedQuickUnionUF ufForPercolation, ufForIsFull;
     private boolean[] opennessArray;
     private int openedSiteCount;
     private int top, down;
 
     public Percolation(int _N) {
         N = _N;
-        uf = new WeightedQuickUnionUF(N * N + 2); // plus top and down
+        ufForPercolation = new WeightedQuickUnionUF(N * N + 2); // plus top and down
+        ufForIsFull = new WeightedQuickUnionUF(N * N + 1); // only the top
         top = N * N;
         down = N * N + 1;
         opennessArray = new boolean[N * N];
         for (int i = 0; i < N; i ++) {
-            uf.union(i, top);
+            ufForPercolation.union(i, top);
+            ufForIsFull.union(i, top);
         }
         for (int i = 0; i < N; i ++) {
-            uf.union(xyToInt(N - 1, i), down);
+            ufForPercolation.union(xyToInt(N - 1, i), down);
         }
         openedSiteCount = 0;
     }
@@ -28,16 +30,20 @@ public class Percolation {
             openedSiteCount ++;
             opennessArray[xyToInt(row, col)] = true;
             if (isCoordinateValid(row - 1, col) && isOpen(row - 1, col)) {
-                uf.union(xyToInt(row, col), xyToInt(row - 1, col));
+                ufForPercolation.union(xyToInt(row, col), xyToInt(row - 1, col));
+                ufForIsFull.union(xyToInt(row, col), xyToInt(row - 1, col));
             }
             if (isCoordinateValid(row + 1, col) && isOpen(row + 1, col)) {
-                uf.union(xyToInt(row, col), xyToInt(row + 1, col));
+                ufForPercolation.union(xyToInt(row, col), xyToInt(row + 1, col));
+                ufForIsFull.union(xyToInt(row, col), xyToInt(row + 1, col));
             }
             if (isCoordinateValid(row, col - 1) && isOpen(row, col - 1)) {
-                uf.union(xyToInt(row, col), xyToInt(row, col - 1));
+                ufForPercolation.union(xyToInt(row, col), xyToInt(row, col - 1));
+                ufForIsFull.union(xyToInt(row, col), xyToInt(row + 1, col));
             }
             if (isCoordinateValid(row, col + 1) && isOpen(row, col + 1)) {
-                uf.union(xyToInt(row, col), xyToInt(row, col + 1));
+                ufForPercolation.union(xyToInt(row, col), xyToInt(row, col + 1));
+                ufForIsFull.union(xyToInt(row, col), xyToInt(row + 1, col));
             }
         }
     }
@@ -50,7 +56,7 @@ public class Percolation {
     public boolean isFull(int row, int col) {
         if (!isCoordinateValid(row, col)) throw new IndexOutOfBoundsException();
         if (!opennessArray[xyToInt(row, col)]) return false;
-        return uf.connected(xyToInt(row, col), top);
+        return ufForIsFull.connected(xyToInt(row, col), top);
     }
 
     public int numberOfOpenSites() {
@@ -58,7 +64,7 @@ public class Percolation {
     }
 
     public boolean percolates() {
-        return uf.connected(top, down);
+        return ufForPercolation.connected(top, down);
     }
 
     private int xyToInt(int row, int col) {
@@ -66,8 +72,7 @@ public class Percolation {
     }
 
     private boolean isCoordinateValid(int row, int col) {
-        if (row < 0 || row >= N || col < 0 || col >= N) return false;
-        return true;
+        return row >= 0 && row < N && col >= 0 && col < N;
     }
 
 }
